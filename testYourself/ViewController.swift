33059d74723677d4123c37eb171f22415c5208b7
Marin Bricq <bricqmarin@yahoo.fr>
2018-02-04 20:00:45 +0100

    Ajoute la possibilité de faire glisser les choix dans le menu principal

--- testYourself/ViewController.swift
+++ testYourself/ViewController.swift
@@ -20,6 +20,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        let screenCenter = self.view.center
+        
+        if let senderView = sender.view {
+            if senderView.center.x + translation.x <= screenCenter.x {
+                senderView.center.x = senderView.center.x + translation.x
+            }
+        
+        
+            if sender.state == UIGestureRecognizerState.ended {
+                UIView.animate(withDuration: 0.5, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                    senderView.center.x = screenCenter.x
+                }, completion: nil)
+            }
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
 }
 
