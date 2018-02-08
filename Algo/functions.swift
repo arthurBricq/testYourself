41dd4d30958c08c251748a08d66c48afc028d6e8
Marin Bricq <bricqmarin@yahoo.fr>
2018-02-08 23:11:52 +0100

    Travail sur le pan de la dernière vue. Il reste encore beaucoup de logique à faire pour que cela fonctionne. Mais on est sur de bonnes voies.

--- Algo/functions.swift
+++ Algo/functions.swift
@@ -9,6 +9,9 @@
 import Foundation
 import UIKit
 
+func norme(vector: CGVector) -> CGFloat {
+    return sqrt((vector.dx)*(vector.dx) + (vector.dy)*(vector.dy))
+}
 func norme(vector: CGPoint) -> CGFloat {
     return sqrt((vector.x)*(vector.x) + (vector.y)*(vector.y))
 }
